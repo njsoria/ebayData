a49a72803a2e29c9ce9c1917e4166f9de9927463
njsoria <nate@devkore.com>
2019-01-13 11:09:55 -0800

    more template dev

--- README.md
+++ README.md
@@ -1,3 +1,30 @@
-# ebayData
+# ebay
 
-clone the repository and npm install
+> A Vue.js project that helps us pull an average price for items. Will have the ability to upload a CSV in the future and spit out average price in the proper column or connect to a google sheet
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+
+# run unit tests
+npm run unit
+
+# run e2e tests
+npm run e2e
+
+# run all tests
+npm test
+```
+
+For detailed explanation on how things work, checkout the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
--- app.js
+++ app.js
@@ -1,67 +1,64 @@
-const request = require("request");
+const request = require('request')
 
 var options = { method: 'GET',
 url: 'https://svcs.ebay.com/services/search/FindingService/v1',
-qs: 
+qs:
 { 'OPERATION-NAME': 'findItemsByKeywords',
     'SECURITY-APPNAME': 'devKore-devkore-PRD-c393cab7d-511d9e96',
     'RESPONSE-DATA-FORMAT': 'JSON',
     keywords: 'DELL 43 ULTRA HD 4K'},
-headers: 
+headers:
 { 'Postman-Token': '712629e4-6ad6-4e7e-ad49-7a3606aafe46',
-    'cache-control': 'no-cache' } 
-};
+    'cache-control': 'no-cache' }
+}
 
-//we'll use request to make the API calls to eBay
-function getPrice(options, callback){
+// we'll use request to make the API calls to eBay
+function getPrice (options, callback){
     request(options, function (error, response, body) {
-        if (error) throw new Error(error);
+        if (error) throw new Error(error)
 
-        //turn the response into a JSON object
-        body = JSON.parse(body);
+        // turn the response into a JSON object
+        body = JSON.parse(body)
 
-        //let's add up the prices we get
-        let price = 0;
+        // let's add up the prices we get
+        let price = 0
 
-        //let's track how many items actually have a listing price
-        let itemCount = 0;
+        // let's track how many items actually have a listing price
+        let itemCount = 0
 
-        //loop through the 100 items returned
-        for(var i=0; i<100; i++){
-            //lets see if we can get a listing price value
-            try{
-                //if we're successful then assign it to an easier to use var
-                let getPrice = body.findItemsByKeywordsResponse[0].searchResult[0].item[i].sellingStatus[0].currentPrice[0].__value__;
-                console.log(i+': '+getPrice);
-                
+        // loop through the 100 items returned
+        for (var i = 0; i < 100; i++){
+            // lets see if we can get a listing price value
+            try {
+                // if we're successful then assign it to an easier to use var
+                let getPrice = body.findItemsByKeywordsResponse[0].searchResult[0].item[i].sellingStatus[0].currentPrice[0].__value__
+                console.log(i + ': ' + getPrice)
                 // as long as the listing price isn't at 0 then let's
                 // add it to the total price
-                if(getPrice > 0){
-                    let add = parseFloat(getPrice);
-                    price = price + add;
+                if (getPrice > 0){
+                    let add = parseFloat(getPrice)
+                    price = price + add
                 }
 
                 // increase item counter
-                itemCount++;
+                itemCount++
             } catch (err) {
                 // let us know if it didn't find a price
-                console.log(i+': no price found');
+                console.log(i + ': no price found')
             }
-
         }
 
         // divide the summed up price the the items added
-        let avgPrice = price/itemCount;
+        let avgPrice = price / itemCount
 
         // show us the price
-        console.log(avgPrice);
+        console.log(avgPrice)
 
-        callback(null, avgPrice);
-    });
+        callback(null, avgPrice)
+    })
 }
 
-getPrice(options, function(err, body){
-    if (err) { console.log(err)}
-    else { console.log(body)}
-});
-
+getPrice(options, function (err, body){
+    if (err) console.log(err)
+    else { console.log(body) }
+})
